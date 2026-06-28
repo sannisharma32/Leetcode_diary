13576c70bd1f762b1efaf5df940b00fb42fb51a3
Sanni kumar <134161612+sannisharma32@users.noreply.github.com>
2026-06-28 15:59:03 +0530

    Time: 11 ms (12.08%) | Memory: 77.6 MB (41.25%) - LeetSync

---
+++ 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
+
+        int n= arr.length;
+
+        Arrays.sort(arr);
+
+        arr[0]=1;
+        for(int i=1;i<n;i++){
+            if(arr[i]>arr[i-1]+1){
+                int count= arr[i]-arr[i-1];
+                arr[i]=arr[i]-count+1;
+            }
+        }
+
+        return arr[n-1];
+        
+    }
+}
